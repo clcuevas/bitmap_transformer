518aa6af44756b8e3ed707deeff92e1df88a09bb
Claudia Cuevas <omordn@gmail.com>
2015-05-01 09:49:15 -0700

    add base files

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.initConfig({
+    jshint: {
+      dev: {
+        src: ['Gruntfile.js', 'server.js', 'test/**/*.js']
+      },
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    simplemocha: {
+      dev: {
+        src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      app: {
+        files: ['<% jshint.dev.src %>', '<%= simplemocha.dev.src %>'],
+        tasks: ['jshint', 'simplemocha']
+      }
+    }
+  });//end grunt initConfig
+  grunt.registerTask('test', ['jshint:dev']);
+  grunt.registerTask('mocha', ['simplemocha:dev']);
+  grunt.registerTask('default', ['jshint', 'mocha', 'watch']);
+};
